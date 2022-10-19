fa1b0698f8bb85379605b9da2ffb4e4a7e20dab1
ShamilHajiyev <shamilnh@code.edu.az>
2022-10-19 18:08:03 +0400

    initial commit

---
+++ Odd-or-even/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Odd_or_even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("\nYour number is even");
+            }
+            else
+            {
+                Console.WriteLine("\nYour number is odd");
+            }
+        }
+    }
+}
